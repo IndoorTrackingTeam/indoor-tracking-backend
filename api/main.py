f80f65638057c17c571d476c07fed6ad84ec0cfc
Fernanda Nagata Ito <fernanda.fn@gec.inatel.br>
2024-07-16 19:09:53 -0300

    refactor: removed room route

--- api/main.py
+++ api/main.py
@@ -1,6 +1,6 @@
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
-from src.app.routes import equipment, user, room
+from src.app.routes import equipment, user
 import uvicorn
 
 api = FastAPI()
@@ -15,7 +15,6 @@ api.add_middleware(
 
 api.include_router(user.router, prefix="/user", tags=["user"])
 api.include_router(equipment.router, prefix="/equipment", tags=["equipment"])
-api.include_router(room.router, prefix="/room", tags=["room"])
 
 if __name__ == "__main__":
     uvicorn.run("main:api", host="0.0.0.0", port=8000, reload=True)
