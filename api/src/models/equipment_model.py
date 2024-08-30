0123ff9394223bcc04d3cf034a7c6f13bde9b714
FerNagata <fernanda.nagata@gec.inatel.br>
2024-08-30 20:25:50 -0300

    chore: created directory for models and moved models to specific files

--- api/src/models/equipment_model.py
+++ api/src/models/equipment_model.py
@@ -1,46 +1,10 @@
 from typing import Annotated, List, Optional
-from pydantic import BaseModel, BeforeValidator, Field, ConfigDict
-from fastapi import UploadFile, File
+from pydantic import BaseModel, BeforeValidator, Field
 from datetime import datetime
 
 # Represents an ObjectId field in the database.
 # It will be represented as a `str` on the model so that it can be serialized to JSON.
 PyObjectId = Annotated[str, BeforeValidator(str)]
-
-# Generic message
-class Message(BaseModel):
-    message: str
-
-# User shared properties
-class UserBase(BaseModel):
-     name: str = Field(...)
-     email: str = Field(...)
-     password: str = Field(...)
-
-class Login(BaseModel):
-    email: str = Field(...)
-    password: str = Field(...)
-
-class UserId(BaseModel):
-    id: str = Field(alias="_id", default=None)
-
-class UserAdmin(BaseModel):
-    email: str = Field(...)
-    isAdmin: bool = Field(default=False)
-
-class UserBasicData(BaseModel):
-    email: Optional[str] = Field(default='')
-    name: Optional[str] = Field(default='')
-    photo: Optional[str] = Field(default='')
-    # image: UploadFile = File(...)
-
-class UserData(UserBasicData):
-    # id: Optional[PyObjectId] = Field(alias="_id", default=None)
-    isAdmin: bool = Field(default=False)
-
-class UpdateUserPhoto(UserId):
-    photo: str = Field(...)
-
 
 class EquipmentBase(BaseModel):
     name: str = Field(...)
@@ -51,7 +15,6 @@ class EquipmentBase(BaseModel):
     esp_id: str = Field(...)
     image: Optional[str] = Field(default=None)
     # esp_id: Optional[str] = Field(default=None)
-
 
 class EquipmentHistoric(BaseModel):
     initial_date: datetime = Field(...)
---
+++ api/src/models/user_model.py
@@ -0,0 +1,38 @@
+from typing import Annotated, List, Optional
+from pydantic import BaseModel, BeforeValidator, Field, ConfigDict
+from fastapi import UploadFile, File
+from datetime import datetime
+
+# Represents an ObjectId field in the database.
+# It will be represented as a `str` on the model so that it can be serialized to JSON.
+PyObjectId = Annotated[str, BeforeValidator(str)]
+
+# User shared properties
+class UserBase(BaseModel):
+     name: str = Field(...)
+     email: str = Field(...)
+     password: str = Field(...)
+
+class Login(BaseModel):
+    email: str = Field(...)
+    password: str = Field(...)
+
+class UserId(BaseModel):
+    id: str = Field(alias="_id", default=None)
+
+class UserAdmin(BaseModel):
+    email: str = Field(...)
+    isAdmin: bool = Field(default=False)
+
+class UserBasicData(BaseModel):
+    email: Optional[str] = Field(default='')
+    name: Optional[str] = Field(default='')
+    photo: Optional[str] = Field(default='')
+    # image: UploadFile = File(...)
+
+class UserData(UserBasicData):
+    # id: Optional[PyObjectId] = Field(alias="_id", default=None)
+    isAdmin: bool = Field(default=False)
+
+class UpdateUserPhoto(UserId):
+    photo: str = Field(...)
