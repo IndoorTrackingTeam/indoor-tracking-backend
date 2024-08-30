ba6e1e21d0cd18ae4e253f3b13746cf6c295bf0c
FerNagata <fernanda.nagata@gec.inatel.br>
2024-08-30 19:33:18 -0300

    test: added integration tests for equipments

---
+++ api/test/utils/mockEquipment.py
@@ -0,0 +1,246 @@
+from datetime import datetime
+
+
+def create_valid_equipments():
+    return [
+        {
+            "name": "Multi Parameter Monitor",
+            "register": "PAT1111",
+            "maintenance": False,
+            "c_room": "Emergency",
+            "c_date": datetime(2024,8,8,19,54,14),
+            "esp_id": "1111",
+            "historic": [
+                {
+                    "initial_date": datetime(2024,8,5,8,0,0),
+                    "room": "Room 20"
+                },
+                {
+                    "initial_date": datetime(2024,8,7,15,34,14),
+                    "room": "Clinic"
+                },
+                {
+                    "initial_date": datetime(2024,8,7,22,12,16),
+                    "room": "Room 14"
+                }
+            ]
+        },
+        {
+            "name": "Defibrillator",
+            "register": "PAT2222",
+            "maintenance": False,
+            "c_room": "Emergency",
+            "c_date": datetime(2024,8,8,19,54,14),
+            "esp_id": "2222"
+        },
+        {
+            "name": "Infusion Pump",
+            "register": "PAT3333",
+            "maintenance": True,
+            "c_room": "Maintenance room",
+            "c_date": datetime(2024,8,8,19,54,14),
+            "esp_id": "3333"
+        }
+    ]
+def valid_equipments_response():
+    return [
+        {
+            "name": "Multi Parameter Monitor",
+            "register": "PAT1111",
+            "maintenance": False,
+            "c_room": "Emergency",
+            "c_date": "2024-08-08T19:54:14",
+            "esp_id": "1111",
+            "image": None
+        },
+        {
+            "name": "Defibrillator",
+            "register": "PAT2222",
+            "maintenance": False,
+            "c_room": "Emergency",
+            "c_date": "2024-08-08T19:54:14",
+            "esp_id": "2222",
+            "image": None
+        },
+        {
+            "name": "Infusion Pump",
+            "register": "PAT3333",
+            "maintenance": True,
+            "c_room": "Maintenance room",
+            "c_date": "2024-08-08T19:54:14",
+            "esp_id": "3333",
+            "image": None
+        }
+    ]
+
+def create_valid_equipment():
+    return {
+            "name": "Wheelchair",
+            "register": "PAT4444",
+            "maintenance": False,
+            "c_room": "Room 2",
+            "c_date": "2024-08-08T22:46:42",
+            "esp_id": "4444"
+        }
+
+def equipment_already_exist():
+    return {
+            "name": "Multi Parameter Monitor",
+            "register": "PAT1111",
+            "maintenance": False,
+            "c_room": "Emergency",
+            "c_date": "2024-08-08T19:54:14",
+            "esp_id": "1111"
+        }
+
+def response_get_one():
+    return {
+            "name": "Multi Parameter Monitor",
+            "register": "PAT1111",
+            "maintenance": False,
+            "c_room": "Emergency",
+            "c_date": "2024-08-08T19:54:14",
+            "esp_id": "1111",
+            "image": None,
+            "historic": [
+                {
+                    "initial_date": "2024-08-05T08:00:00",
+                    "room": "Room 20"
+                },
+                {
+                    "initial_date": "2024-08-07T15:34:14",
+                    "room": "Clinic"
+                },
+                {
+                    "initial_date": "2024-08-07T22:12:16",
+                    "room": "Room 14"
+                }
+            ]
+        }
+
+def equipments_in_same_room():
+    return [
+        {
+            "name": "Multi Parameter Monitor",
+            "register": "PAT1111",
+            "maintenance": False,
+            "c_room": "Emergency",
+            "c_date": "2024-08-08T19:54:14",
+            "esp_id": "1111",
+            "image": None,
+            "historic": [
+                {
+                    "initial_date": "2024-08-05T08:00:00",
+                    "room": "Room 20"
+                },
+                {
+                    "initial_date": "2024-08-07T15:34:14",
+                    "room": "Clinic"
+                },
+                {
+                    "initial_date": "2024-08-07T22:12:16",
+                    "room": "Room 14"
+                }
+            ]
+        },
+        {
+            "name": "Defibrillator",
+            "register": "PAT2222",
+            "maintenance": False,
+            "c_room": "Emergency",
+            "c_date": "2024-08-08T19:54:14",
+            "esp_id": "2222",
+            "historic": None,
+            "image": None
+        }
+    ]
+
+def valid_update_mainteinance():
+    return {
+        "register": "PAT2222",
+        "maintenance": True
+    }
+
+def invalid_update_mainteinance():
+    return {
+        "register": "invalid_register",
+        "maintenance": True
+    }
+
+def response_historic():
+    return [
+        {
+            "name": "Multi Parameter Monitor",
+            "register": "PAT1111",
+            "historic": [
+                {
+                    "initial_date": "2024-08-05T08:00:00",
+                    "room": "Room 20"
+                },
+                {
+                    "initial_date": "2024-08-07T15:34:14",
+                    "room": "Clinic"
+                },
+                {
+                    "initial_date": "2024-08-07T22:12:16",
+                    "room": "Room 14"
+                }
+            ]
+        },
+        {
+            "name": "Defibrillator",
+            "register": "PAT2222",
+            "historic": None
+        },
+        {
+            "name": "Infusion Pump",
+            "register": "PAT3333",
+            "historic": None
+        }
+    ]
+
+def response_current_room_date():
+    return {
+            "name": "Defibrillator",
+            "register": "PAT2222",
+            "c_room": "Emergency",
+            "c_date": "2024-08-08T19:54:14",
+        }
+
+def valid_esp_id_room():
+    return {
+        "esp_id": "3333",
+        "c_room": "Emergency"
+    }
+
+def invalid_esp_id_room():
+    return {
+        "esp_id": "invalid",
+        "c_room": "Emergency"
+    }
+
+def valid_update_historic():
+    return {
+        "esp_id": "3333",
+        "room": "Maintenance room",
+        "initial_date": "2024-08-08T19:54:14"
+    }
+
+def invalid_update_historic():
+    return {
+        "esp_id": "invalid",
+        "room": "Maintenance room",
+        "initial_date": "2024-08-08T19:54:14"
+    }
+
+def valid_update_image():
+    return {
+  "register": "PAT2222",
+  "image": "iVBORw0KGgoAAAANSUhEUg-image-exemple-not-real"
+}
+
+def invalid_update_image():
+    return {
+  "register": "invalid_register",
+  "image": "iVBORw0KGgoAAAANSUhEUg-image-exemple-not-real"
+}
