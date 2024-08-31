ed37ddd85df65f02765026e403c3eacea78f6733
FerNagata <fernanda.nagata@gec.inatel.br>
2024-08-31 10:24:56 -0300

    feat: added variable mac to RouterData class

--- api/src/models/router_data.py
+++ api/src/models/router_data.py
@@ -3,6 +3,7 @@ from typing import Optional
 from pydantic import BaseModel, Field
 
 class RouterData(BaseModel):
+    mac: str = Field(...)
     esp_id: str = Field(...)
     rssi: int = Field(...)
     date: Optional[datetime] = Field(default=datetime.now())
