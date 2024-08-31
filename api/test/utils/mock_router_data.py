e8299d048f3b06fc7ea92da0cb0bd09a396ac200
FerNagata <fernanda.nagata@gec.inatel.br>
2024-08-31 10:25:45 -0300

    test: added varable mac to router data (and training) mock

--- api/test/utils/mock_router_data.py
+++ api/test/utils/mock_router_data.py
@@ -1,5 +1,6 @@
 def valid_router_training_data():
     return {
+        "mac": "34:80:D2:02:FA:C8",
         "esp_id": "4444",
         "rssi": -50,
         "room": "Emergency"
@@ -7,18 +8,21 @@ def valid_router_training_data():
 
 def invalid_router_training_data():
     return {
+        "mac": "34:80:D2:02:FA:C8",
         "esp_id": "4444",
         "rssi": "fifty",
         "room": "Emergency"
     }
 def valid_router_data():
     return {
+        "mac": "34:80:D2:02:FA:C8",
         "esp_id": "4444",
         "rssi": -50,
     }
 
 def invalid_router_data():
     return {
+        "mac": "34:80:D2:02:FA:C8",
         "esp_id": "4444",
         "rssi": "fifty",
     }
