da0a646cba9994fa2b0ce70b47f1b6cbcd754721
FerNagata <fernanda.nagata@gec.inatel.br>
2024-09-03 15:24:34 -0300

    fix: fixed create and update function of the router training data document

--- api/src/database/repositories/router_training_data_repository.py
+++ api/src/database/repositories/router_training_data_repository.py
@@ -7,12 +7,25 @@ class RouterTrainingDataDAO: # DAO - Data Access Object
 
     def create(self, data: RouterTrainingData):
         try:
-            result_update = self.db.collection.update_one({"name": "training data"}, {'$push': {'historic': data.model_dump()}})
+            result_update = self.db.collection.update_one({"mac": data.mac}, {'$push': {'historic': {
+                            "esp_id": data.esp_id,
+                            "date": data.date,
+                            "room": data.room,
+                            "rssi": data.rssi
+                        }}})
 
             if result_update.matched_count == 0:
                 new_data = {
-                    "name": "training data",
-                    "historic": [data.model_dump()]  # Inicializando 'historic' como uma lista com o objeto
+                    "mac": data.mac,
+                    "name_router": data.name_router,
+                    "historic": [
+                        {
+                            "esp_id": data.esp_id,
+                            "date": data.date,
+                            "room": data.room,
+                            "rssi": data.rssi
+                        }
+                    ]  # Inicializando 'historic' como uma lista com o objeto
                 }
                 result = self.db.collection.insert_one(new_data)
             
