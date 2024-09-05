7b2a9291af94fbfa0b3724e854cc0d76c4b6a972
FerNagata <fernanda.nagata@gec.inatel.br>
2024-09-05 10:01:17 -0300

    test: fixed request body

--- api/test/utils/mock_router_data.py
+++ api/test/utils/mock_router_data.py
@@ -1,32 +1,89 @@
 def valid_router_training_data():
     return {
-        "esp_id": "4444",
-        "rssi": -50,
-        "mac": "34:80:D2:02:FA:C8",
-        "name_router": "hospital",
-        "room": "Emergency"
+        "room": "Room 20",
+        "networks": [
+             {
+                "mac": "34:80:D2:02:FA:C8",
+                "name_router": "hospital",
+                "rssi": -94,
+                "esp_id": "1111"
+            },
+            {
+                "mac": "34:82:B2:02:FA:C6",
+                "name_router": "hospital-wifi",
+                "rssi": -87,
+                "esp_id": "1111"
+            },
+            {
+                "mac": "12:33:C2:02:00:FA",
+                "name_router": "hospital-wifi2",
+                "rssi": -60,
+                "esp_id": "1111"
+            }
+        ]
     }
 
 def invalid_router_training_data():
     return {
-        "mac": "34:80:D2:02:FA:C8",
-        "esp_id": "4444",
-        "rssi": "fifty",
-        "name_router": "hospital",
-        "room": "Emergency"
-    }
-def valid_router_data():
-    return {
-        "mac": "34:80:D2:02:FA:C8",
-        "esp_id": "4444",
-        "name_router": "hospital",
-        "rssi": -50,
+        "room": "Room 20",
+        "networks": [
+             {
+                "mac": "34:80:D2:02:FA:C8",
+                "name_router": "hospital",
+                "rssi": -94,
+            },
+            {
+                "mac": "34:82:B2:02:FA:C6",
+                "name_router": "hospital-wifi",
+                "rssi": -87,
+            },
+            {
+                "mac": "12:33:C2:02:00:FA",
+                "name_router": "hospital-wifi2",
+                "rssi": -60,
+            }
+        ]
     }
 
-def invalid_router_data():
+def valid_router_data():
     return {
-        "mac": "34:80:D2:02:FA:C8",
-        "esp_id": "4444",
-        "name_router": "hospital",
-        "rssi": "fifty",
-    }
+            "esp_id": "1111",
+            "networks": [
+                {
+                "mac": "34:80:D2:02:FA:C8",
+                "name_router": "hospital",
+                "rssi": -94
+                },
+                {
+                "mac": "34:82:B2:02:FA:C6",
+                "name_router": "hospital-wifi",
+                "rssi": -87
+                },
+                {
+                "mac": "12:33:C2:02:00:FA",
+                "name_router": "hospital-wifi2",
+                "rssi": -60
+                }
+            ]
+        }
+
+def invalid_router_data():
+    return { 
+            "networks": [
+                {
+                "mac": "34:80:D2:02:FA:C8",
+                "name_router": "hospital",
+                "rssi": -94
+                },
+                {
+                "mac": "34:82:B2:02:FA:C6",
+                "name_router": "hospital-wifi",
+                "rssi": -87
+                },
+                {
+                "mac": "12:33:C2:02:00:FA",
+                "name_router": "hospital-wifi2",
+                "rssi": -60
+                }
+            ]
+        }
