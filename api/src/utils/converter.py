3e0f249ecb410affa743f063d4eb3d624306554c
FerNagata <fernanda.nagata@gec.inatel.br>
2024-09-16 20:39:35 -0300

    hotfix: fixed date bug on conversion

--- api/src/utils/converter.py
+++ api/src/utils/converter.py
@@ -7,7 +7,7 @@ class Message(BaseModel):
     message: str
     
 def parse_date(date_str):
-        for fmt in ("%Y-%m-%dT%H:%M:%S.%fZ", "%Y-%m-%dT%H:%M:%SZ"):
+        for fmt in ("%Y-%m-%dT%H:%M:%S.%fZ", "%Y-%m-%dT%H:%M:%SZ", "%Y-%m-%d %H:%M:%S"):
             try:
                 return datetime.strptime(date_str, fmt)
             except ValueError:
@@ -16,7 +16,13 @@ def parse_date(date_str):
 
 def convert_mongo_document(doc):
     if 'c_date' in doc:
-         doc['c_date'] = parse_date(doc['c_date']['$date'])
+        #  doc['c_date'] = parse_date(doc['c_date']['$date'])
+        if isinstance(doc.get('c_date'), dict) and '$date' in doc['c_date']:
+            # Acessa a data a partir do formato dicionário
+            doc['c_date'] = parse_date(doc['c_date']['$date'])
+        elif isinstance(doc.get('c_date'), str):
+            # Converte diretamente se já for uma string
+            doc['c_date'] = parse_date(doc['c_date'])
     if '_id' in doc:
         doc['id'] = str(doc['_id']['$oid'])
     if 'historic' in doc and doc['historic'] is not None:
